361ba00a6b90738356039e1bf38839bbe1bd4523
Wanjiru12 <cmwai774@gmail.com>
2024-05-15 16:06:15 +0300

    modfying the API

--- src/bike.js
+++ src/bike.js
@@ -1,11 +1,14 @@
 // bike.js
 export default class StolenBike {
     static getStolenBike(location) {
-        const url = `https://bikeindex.org/api/v3/search?page=1&per_page=10&location=${location}&distance=10&stolenness=proximity`;
+        const oneWeekAgo = Math.floor(Date.now() / 1000) - 7 * 24 * 60 * 60;
+        const url = `https://bikeindex.org/api/v3/search?page=1&per_page=100&location=${location}&distance=10&stolenness=proximity&stolen_after=${oneWeekAgo}`;
         return fetch(url)
             .then((response) => {
                 if (!response.ok) {
-                    throw new Error("Network response was not ok " + response.statusText);
+                    throw new Error(
+                        `Network response was not ok: ${response.statusText}`
+                    );
                 }
                 return response.json();
             })
