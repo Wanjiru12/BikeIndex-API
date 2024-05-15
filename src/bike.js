c96c796fbd9d608ffe5b318a493edcb431174766
Wanjiru12 <cmwai774@gmail.com>
2024-05-15 15:19:52 +0300

    location change

--- src/bike.js
+++ src/bike.js
@@ -1,19 +1,19 @@
-
 //Business Logic
 export default class StolenBike {
-  static getStolenBike(location) {
-    return fetch(`https://bikeindex.org/api/v3/search?page=1&per_page=25&location=${location}&distance=20&stolenness=proximity&${process.env.API_KEY}`)
-      .then(function (response) {
-        if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}`;
-          throw new Error(errorMessage);
-        } else {
-          return response.json();
-        }
-      })
-      .catch(function (error) {
-        return error.message;
-      });
-  }
-
+    static getStolenBike(location) {
+        return fetch(
+                `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=Chicago&distance=10&stolenness=proximity${process.env.API_KEY}`
+            )
+            .then(function(response) {
+                if (!response.ok) {
+                    const errorMessage = `${response.status} ${response.statusText}`;
+                    throw new Error(errorMessage);
+                } else {
+                    return response.json();
+                }
+            })
+            .catch(function(error) {
+                return error.message;
+            });
+    }
 }
